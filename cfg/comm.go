a4775bba5556369e42a68c2f58631f385458a310
_dyl <33545159+weq95@users.noreply.github.com>
2023-07-12 18:19:39 +0800

    添加监听程序退出命令

--- cfg/comm.go
+++ cfg/comm.go
@@ -38,3 +38,27 @@ func HttpGet(url string, result interface{}) error {
 
 	return json.Unmarshal(body, result)
 }
+
+// ExitApplication 手动退出应用
+func ExitApplication() {
+	var app, err = os.FindProcess(os.Getpid())
+	if err != nil {
+		Log.Error(err.Error())
+		return
+	}
+	if err = app.Signal(os.Interrupt); err != nil {
+		// Windows 需要在这里处理退出清理逻辑
+		var handler, _ = syscall.GetCurrentProcess()
+		if err = syscall.TerminateProcess(handler, 0); err != nil {
+			Log.Error("Windows Failed to terminate process " + err.Error())
+		}
+	}
+}
+
+// WaitCtrlC 创建等待退出管道
+func WaitCtrlC() <-chan os.Signal {
+	var sigCh = make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM, syscall.SIGINT, syscall.SIGKILL)
+
+	return sigCh
+}
