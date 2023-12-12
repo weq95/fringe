09eae45e57ce7bbb468f3c44850b0addb236ba02
_dyl <33545159+weq95@users.noreply.github.com>
2023-12-12 14:02:20 +0800

    Update .sh

--- .sh
+++ .sh
@@ -14,7 +14,7 @@ echo -e "项目 [wallet] 编译成功"
 
 
 # 应用程序数组
-apps=("wallet8070")
+apps=("wallet8070" "wallet8071")
 
 # 循环处理每个服务
 for service in "${apps[@]}"; do
@@ -24,10 +24,10 @@ for service in "${apps[@]}"; do
         processNum=$(ps -ef | grep $service | grep -v grep | wc -l)
         if [ $processNum -eq 1 ]; then
             echo -e "开始执行终止 [$service] 进程"
-            ps -aux | grep "$service" | awk '{print $2}' | xargs kill
+            ps -aux | grep $service | grep -v grep | awk '{print $2}' | xargs kill
             sleep 1
         else
-            echo -e "$service 进程终止, $service 停止服务"
+            echo -e "$service 进程已经终止, $service 停止服务"
             break
         fi
         sleep 1
@@ -40,26 +40,32 @@ for service in "${apps[@]}"; do
     fi
 
     # 复制最新启动包
+    service=$(echo "$service" | tr -d '[:space:]')
     cp "/opt/$appName" "/opt/$portDir/$service"
     chmod 755 "/opt/$portDir/$service"
     echo -e "$service 最新启动包复制成功"
 
-while true; do
-    processNum=$(ps -ef | grep $service | grep -v grep | wc -l)
-    if [ $processNum -eq 0 ]; then
-       echo -e "/opt/$portDir/$service > /dev/null 2>&1 &"
-       nohup /opt/$portDir/$service > /dev/null 2>&1 &
-       echo -e "command executed successfully."
-
-    else
-      serviceInfo=$(ps -ef | grep "$service")
-      echo -e "service-info[$service]: \r\r$serviceInfo\r"
-      echo -e "start $service success. 服务已启动, 执行后续操作"
-      # 查看日志命令
-      echo -e "tail -f /opt/$portDir/$service/logs/$(date +%Y%m%d).log -n 50"
-      echo -e "------------------ 进入15秒等待状态... ------------------"
-      sleep 15
-      break
-    fi
+    while true; do
+        processNum=$(ps -ef | grep $service | grep -v grep | wc -l)
+        if [ $processNum -eq 0 ]; then
+           echo -e "/opt/$portDir/$service > /dev/null 2>&1 &"
+           # shellcheck disable=SC2164
+           cd "/opt/$portDir/"
+           nohup ./$service > /dev/null 2>&1 &
+           echo -e "command executed successfully."
+           sleep 1
+        else
+          serviceInfo=$(ps -ef | grep "$service | grep -v grep")
+          echo -e "service-info[$service]: \r\r$serviceInfo\r"
+          echo -e "start $service success. 服务已启动, 执行后续操作"
+          # 查看日志命令
+          echo -e "tail -f /opt/$portDir/logs/$(date +%Y-%m-%d).log -n 50"
+          echo -e "------------------ 进入15秒等待状态... ------------------"
+          sleep 15
+          break
+        fi
+    done
 done
-done
+serviceInfo=$(ps -ef | grep "$appName" | grep -v grep)
+echo -e "\r\r------------------> 服务已全部启动 <------------------"
+echo -e "service-info[$appName]: \r\r$serviceInfo\r\r"
