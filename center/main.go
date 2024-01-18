880959fdc4e9335fc8eeb9183769f8cfd2c62259
_dyl <33545159+weq95@users.noreply.github.com>
2024-01-18 19:33:24 +0800

    Update main.go

--- center/main.go
+++ center/main.go
@@ -1,52 +1,127 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	"fringe/center/business"
 	"fringe/center/router"
 	"fringe/cfg"
 	"fringe/middleware"
 	"fringe/netsrv"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+	"net/http"
 	"os"
+	"os/signal"
 	"strconv"
+	"time"
 )
 
 func main() {
-	cfg.LoggerDefault()
-
 	var tcpSrvAddr string
 	var port string
+	var logLevel int8
 	_ = cfg.Val(func(cfg *cfg.AppCfg) interface{} {
 		tcpSrvAddr = cfg.ServerTcpAddr
 		port = ":" + strconv.Itoa(int(cfg.ServerHttpPort))
+		logLevel = cfg.LogLevel
+		gin.SetMode(cfg.Environment)
 
 		return nil
 	})
+	_ = cfg.NewLogger(logLevel)
+	defer func() {
+		_ = zap.L().Sync()
+	}()
 	var m, err = netsrv.GetManager().AddServer(
 		map[string]netsrv.TcpClients{
 			tcpSrvAddr: router.NewSrvRouter(),
 		})
 	if err != nil {
-		cfg.Log.Error(err.Error())
+		zap.L().Error(err.Error())
 		os.Exit(1)
 	}
 	defer m.ServerClosed()
 
 	if err = cfg.NewDatabase(); err != nil {
-		cfg.Log.Error(err.Error())
+		zap.L().Error(err.Error())
 		os.Exit(1)
 	}
 	if err = cfg.NewRedisClient(); err != nil {
-		cfg.Log.Error(err.Error())
+		zap.L().Error(err.Error())
 		os.Exit(1)
 	}
 	defer cfg.ClosedRedis()
 
 	business.SyncData()
-	var r = gin.Default()
+	var r = gin.New()
+	r.Use(
+		gin.Logger(),
+		func(c *gin.Context) {
+			method := c.Request.Method
+			origin := c.Request.Header.Get("Origin")
+			if origin != "" {
+				c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+				c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE,UPDATE")
+				c.Header("Access-Control-Allow-Headers", "Authorization, content-type, Content-Length, X-CSRF-Token, Token,session,Access-Control-Allow-Headers,account")
+				c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers")
+				c.Header("Access-Control-Max-Age", "172800")
+				c.Header("Access-Control-Allow-Credentials", "true")
+				c.Set("Content-Type", "application/json")
+			}
+
+			if method == "OPTIONS" {
+				c.JSON(http.StatusNoContent, nil)
+				return
+			}
+
+			c.Next()
+		},
+		func(c *gin.Context) {
+			var startTime = time.Now()
+			c.Next()
+			var duration = time.Now().Sub(startTime)
+			if duration > time.Second*1 {
+				zap.L().Warn("[HttpWeb] 慢请求",
+					zap.String("url", c.Request.URL.String()),
+					zap.String("method", c.Request.Method),
+				)
+			}
+		},
+		gin.CustomRecoveryWithWriter(gin.DefaultErrorWriter, func(c *gin.Context, err any) {
+			if err != nil {
+				return
+			}
+
+			c.JSON(http.StatusOK, gin.H{"code": http.StatusInternalServerError, "message": "server error"})
+			c.Abort()
+		}),
+	)
 	r.POST("/login", business.Login)
 	r.Use(middleware.LoginMiddleware())
-	if err = r.Run(port); err != nil {
-		cfg.Log.Fatal(err.Error())
+
+	var srv = &http.Server{
+		Addr:    port,
+		Handler: r,
 	}
+	go func() {
+		if err = srv.ListenAndServe(); err != nil && !errors.Is(http.ErrServerClosed, err) {
+			zap.L().Fatal(fmt.Sprintf("listen：%+v", err))
+		}
+	}()
+
+	var quit = make(chan os.Signal, 1)
+
+	signal.Notify(quit, os.Interrupt, os.Kill)
+
+	<-quit
+	zap.L().Info("Shutdown Server ...")
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	if err = srv.Shutdown(ctx); err != nil {
+		zap.L().Fatal(fmt.Sprintf("Server Shutdown: %v", err))
+	}
+	zap.L().Info("Server exiting")
 }
