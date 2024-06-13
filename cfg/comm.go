2cf3228b6ae15af99a6071490fa54119b51fa37f
_dyl <33545159+weq95@users.noreply.github.com>
2024-06-13 14:25:36 +0800

    Update comm.go
    添加时间转换函数

--- cfg/comm.go
+++ cfg/comm.go
@@ -56,6 +56,31 @@ func WaitCtrlC() <-chan os.Signal {
 	return sigCh
 }
 
+// YMDHMS 转换为：年月日.时分秒
+func YMDHMS(d time.Duration) string {
+	var (
+		tsecs = d / time.Second
+		tmins = tsecs / 60
+		thrs  = tmins / 60
+		tdays = thrs / 24
+		tyrs  = tdays / 365
+	)
+
+	if tyrs > 0 {
+		return fmt.Sprintf("%dy%dd%dh%dm%ds", tyrs, tdays%365, thrs%24, tmins%60, tsecs%60)
+	}
+	if tdays > 0 {
+		return fmt.Sprintf("%dd%dh%dm%ds", tdays, thrs%24, tmins%60, tsecs%60)
+	}
+	if thrs > 0 {
+		return fmt.Sprintf("%dh%dm%ds", thrs, tmins%60, tsecs%60)
+	}
+	if tmins > 0 {
+		return fmt.Sprintf("%dm%ds", tmins, tsecs%60)
+	}
+	return fmt.Sprintf("%ds", tsecs)
+}
+
 // Yuan2Fen CNY：元转分
 func Yuan2Fen(yuan float64) int64 {
 	return int64(math.Ceil(yuan * 100))
